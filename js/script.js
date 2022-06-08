b329f8109270b35ea5edd19fd381d79cfdb97b60
vladlesun <vladlesunmma@gmail.com>
2022-06-08 22:57:52 +0300

    blanchard finish

--- js/script.js
+++ js/script.js
@@ -97,6 +97,62 @@ setSearch({
 });
 
 
+// Плавный скролл
+document.querySelectorAll('.js-scroll-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const href = this.getAttribute('href').substring(1);
+      const scrollTarget = document.getElementById(href);
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+      window.scrollBy({
+          top: elementPosition,
+          behavior: 'smooth'
+      });
+  })});
+
+// Для мобильных устройств
+(() => {
+	const MOBILE_WIDTH = 580;
+
+	function getWindowWidth () {
+	  return Math.max(
+	    document.body.scrollWidth,
+	    document.documentElement.scrollWidth,
+	    document.body.offsetWidth,
+	    document.documentElement.offsetWidth,
+	    document.body.clientWidth,
+	    document.documentElement.clientWidth
+	  );
+	}
+
+	function scrollToContent (link, isMobile) {
+		if (isMobile && getWindowWidth() > MOBILE_WIDTH) {
+			return;
+		}
+
+	  const href = link.getAttribute('href').substring(1);
+	  const scrollTarget = document.getElementById(href);
+	  const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+	  window.scrollBy({
+	      top: elementPosition,
+	      behavior: 'smooth'
+	  });
+	}
+
+	document.querySelectorAll('.js-scroll-link').forEach(link => {
+	  link.addEventListener('click', function(e) {
+	      e.preventDefault();
+
+	      scrollToContent(this, true);
+	  });
+	});
+})();
+
+
+
 // DROPDOWN
 const params = {
   btnClassName: "js-header-dropdown-btn",
